a3d08ebad251003cc5dfaa9bb3bd7d70047aea25
Vitaliy <v.a.grahov@ya.ru>
2021-04-22 00:27:47 +0500

    Создан объект с данными и операциями

--- app/src/main/java/com/example/gb_calculator/ui/CalculatorActivity.java
+++ app/src/main/java/com/example/gb_calculator/ui/CalculatorActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.gb_calculator.R;
+import com.example.gb_calculator.domain.CalculatorData;
 import com.example.gb_calculator.domain.CalculatorImpl;
 import com.example.gb_calculator.domain.Operation;
 
@@ -21,24 +22,23 @@ public class CalculatorActivity extends AppCompatActivity implements CalculatorV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        presenter = new CalculatorPresenter(this, new CalculatorImpl());
+        presenter = new CalculatorPresenter(this, new CalculatorImpl(), new CalculatorData());
 
         resultText = findViewById(R.id.text_result);
 
         findViewById(R.id.btn_clear).setOnClickListener(v -> presenter.onButtonClearClicked());
 
-        findViewById(R.id.btn_0).setOnClickListener(v -> presenter.onButtonDigitClicked(0));
-        findViewById(R.id.btn_1).setOnClickListener(v -> presenter.onButtonDigitClicked(1));
-        findViewById(R.id.btn_2).setOnClickListener(v -> presenter.onButtonDigitClicked(2));
-        findViewById(R.id.btn_3).setOnClickListener(v -> presenter.onButtonDigitClicked(3));
-        findViewById(R.id.btn_4).setOnClickListener(v -> presenter.onButtonDigitClicked(4));
-        findViewById(R.id.btn_5).setOnClickListener(v -> presenter.onButtonDigitClicked(5));
-        findViewById(R.id.btn_6).setOnClickListener(v -> presenter.onButtonDigitClicked(6));
-        findViewById(R.id.btn_7).setOnClickListener(v -> presenter.onButtonDigitClicked(7));
-        findViewById(R.id.btn_8).setOnClickListener(v -> presenter.onButtonDigitClicked(8));
-        findViewById(R.id.btn_9).setOnClickListener(v -> presenter.onButtonDigitClicked(9));
-
-        findViewById(R.id.btn_dot).setOnClickListener(v -> presenter.onButtonDotClicked());
+        findViewById(R.id.btn_0).setOnClickListener(v -> presenter.onButtonDigitClicked("0"));
+        findViewById(R.id.btn_1).setOnClickListener(v -> presenter.onButtonDigitClicked("1"));
+        findViewById(R.id.btn_2).setOnClickListener(v -> presenter.onButtonDigitClicked("2"));
+        findViewById(R.id.btn_3).setOnClickListener(v -> presenter.onButtonDigitClicked("3"));
+        findViewById(R.id.btn_4).setOnClickListener(v -> presenter.onButtonDigitClicked("4"));
+        findViewById(R.id.btn_5).setOnClickListener(v -> presenter.onButtonDigitClicked("5"));
+        findViewById(R.id.btn_6).setOnClickListener(v -> presenter.onButtonDigitClicked("6"));
+        findViewById(R.id.btn_7).setOnClickListener(v -> presenter.onButtonDigitClicked("7"));
+        findViewById(R.id.btn_8).setOnClickListener(v -> presenter.onButtonDigitClicked("8"));
+        findViewById(R.id.btn_9).setOnClickListener(v -> presenter.onButtonDigitClicked("9"));
+        findViewById(R.id.btn_dot).setOnClickListener(v -> presenter.onButtonDigitClicked("."));
 
         findViewById(R.id.btn_plus).setOnClickListener(v -> presenter.onButtonOperationClicked(Operation.ADD));
         findViewById(R.id.btn_minus).setOnClickListener(v -> presenter.onButtonOperationClicked(Operation.SUB));
--- app/src/main/java/com/example/gb_calculator/ui/CalculatorPresenter.java
+++ app/src/main/java/com/example/gb_calculator/ui/CalculatorPresenter.java
@@ -1,6 +1,7 @@
 package com.example.gb_calculator.ui;
 
 import com.example.gb_calculator.domain.Calculator;
+import com.example.gb_calculator.domain.CalculatorData;
 import com.example.gb_calculator.domain.Operation;
 
 public class CalculatorPresenter {
@@ -9,52 +10,35 @@ public class CalculatorPresenter {
 
     private Calculator calculator;
 
-    private Operation savedOperation = Operation.NULL;
-
-    private String argOne = "";
-    private String argTwo = "";
+    private CalculatorData data;
 
     private String textResult = "";
 
-    public CalculatorPresenter(CalculatorView view, Calculator calculator) {
+    public CalculatorPresenter(CalculatorView view, Calculator calculator, CalculatorData data) {
         this.view = view;
         this.calculator = calculator;
+        this.data = data;
     }
 
     public void onButtonClearClicked() {
-        argOne = "";
-        argTwo = "";
+        data.reset();
         textResult = "";
-        savedOperation = null;
-
         view.showResult(textResult);
     }
 
-    public void onButtonDigitClicked(int digit) {
-        argTwo += digit;
-
-        view.showResult(argTwo);
-    }
-
-    public void onButtonDotClicked() {
-        argTwo += ".";
-
-        view.showResult(argTwo);
+    public void onButtonDigitClicked(String digit) {
+        textResult = data.inputDigit(digit);
+        view.showResult(textResult);
     }
 
     public void onButtonOperationClicked(Operation operation) {
-        if (!argOne.equals("") && savedOperation != Operation.NULL) {
-            double arg1 = Double.parseDouble(argOne);
-            double arg2 = Double.parseDouble(argTwo);
-            double result = calculator.binaryOperation(arg1, arg2, savedOperation);
+        if (data.hasArgOne() && data.hasOperation()) {
+            double result = calculator.binaryOperation(data.getArgOne(), data.getArgTwo(), data.getOperation());
             textResult = String.valueOf(result);
-            view.showResult(textResult);
-            argOne = textResult;
-        } else {
-            argOne = argTwo;
+            data.setDigits(textResult);
         }
-        savedOperation = operation;
-        argTwo = "";
+        data.inputOperation(operation);
+        view.showResult(textResult);
     }
 
 }
